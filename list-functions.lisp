c8a5f4f2a28f113c85515c2b495d090c859afdca
Liam Howley <liam.howley@hotmail.com>
2021-07-26 10:56:34 +0100

    updating map-tree-breadth-first to use basic queue

--- list-functions.lisp
+++ list-functions.lisp
@@ -43,19 +43,27 @@ Returns flattened results."
 		   (t (walk (car inner) (walk (cdr inner) acc))))))
     (walk list nil)))
 
-(defun map-tree-breadth-first (fn list)
-  "Breadth first traversal. Requires a function that takes a single argument. Returns flattened results."
-  (declare (optimize (speed 3) (safety 0)))
-  (labels ((walk (inner acc)
-	     (cond ((null inner)
-		    acc)
-		   ((atom inner)
-		    (cons (funcall fn inner) acc))
-		   ((atom (car inner))
-		    (walk (car inner) (walk (cdr inner) acc)))
-		   (t (walk (cdr inner) (walk (car inner) acc))))))
-    (walk list nil)))
 
+(defun map-tree-breadth-first (fn list)
+  "Breadth first traversal. Requires a function that takes a single argument. 
+Returns flattened results."
+  (let ((values)
+	(children))
+    (flet ((walk (inner)
+	     (loop for item in inner
+		if (atom item)
+		do (let ((result (funcall (the function fn) item)))
+		     (when result
+		       (push result values)))
+		else collect item into queue
+		finally (setf children (append children queue)))))
+      (walk list)
+      (loop
+	 while children
+	 for child = (pop children)
+	 do (walk child)
+	 finally (return (nreverse values))))))
+	 
 
 (defun find-in-tree (value tree &key (test #'equal))
   "Recursively walks through a TREE testing VALUE against
