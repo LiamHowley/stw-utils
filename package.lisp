5126cfc38330575219155e9c64427fb1beeea651
Liam Howley <liam.howley@hotmail.com>
2022-09-11 17:45:11 +0100

    Indenting - quirk of emacs. Additional exporting of radix-trie accessor functions, removal of queue.lisp symbols.

--- package.lisp
+++ package.lisp
@@ -1,79 +1,98 @@
 (defpackage stw.util
   (:use :cl)
-  (:export :scase
-	   :awhile
-	   :aif
-	   :awhen
-	   :self
-	   :with-gensyms
+  (:export
+   :scase
+   :awhile
+   :aif
+   :awhen
+   :self
+   :with-gensyms
 
-	   ;; control flow
-	   :xor
+   ;; control flow
+   :xor
 
-	   ;; queue
-	   :queue
-	   :with-queue
-	   :enqueue
-	   :dequeue
-	   :next-in-queue
-	   :queue-contents
-	   :queue-append
-	   :queue-empty-p
-	   
-	   ;; trie
-	   :trie
-	   :make-trie
-	   :next-character
-	   :find-word
-	   :find-in-string
-	   :insert-character
-	   :insert-word
-	   :delete-word
-	   :trie-branch
-	   :trie-leaf
-	   :walk-branch
-	   :walk-trie
-	   :walk-leaves
-	   :optimize-edge
+   ;; trie
+   :trie
+   :make-trie
+   :next-character
+   :find-word
+   :find-in-string
+   :insert-character
+   :insert-word
+   :delete-word
+   :trie-branch
+   :trie-leaf
+   :walk-branch
+   :walk-trie
+   :walk-leaves
+   :compress-edge
 
-	   
-	   ;; lists
-	   :flatten
-	   :reverse-flatten
-	   :map-tree-depth-first
-	   :map-tree-breadth-first
-	   :mappend
-	   :find-in-tree
-	   :assoc-all
-	   :ensure-list
+   ;;radix-trie
+   :radix-trie
+   :radix-trie-word
+   :radix-trie-leaf
+   :radix-trie-branch
+   :radix-trie-subseq
+   
+   ;; lists
+   :flatten
+   :reverse-flatten
+   :map-tree-depth-first
+   :map-tree-breadth-first
+   :mappend
+   :find-in-tree
+   :assoc-all
+   :ensure-list
+   :ordered-plist-values
+   :number-range
+   :set-difference%
+   :array-to-list
 
-	   ;; strings
-	   :whitespacep
-	   :newlinep
-	   :empty-string/newline-p
-	   :ensure-string
-	   :concat-string
-	   :match-tokens
-	   :match-token
-	   :find-all
-	   :explode-string
-	   :map-exploding-string
-	   :find-and-replace
-	   :make-displaced-array
+   ;; lexer
+   :*document*
+   :*char-index*
+   :*line-number*
+   :*length* 
+   :*decoder*
+   :*consume-whitespace*
+   :stw-read-char
+   :next
+   :stw-peek-next-char
+   :stw-peek-last-char
+   :match-string
+   :match-character
+   :consume-until
+   :consume-while
+   :read-until
+   :read-and-decode
 
-	   ;; files/directories/io
-	   :walk-directory
-	   :list-files
-	   :directory-p
-	   :file-p
-	   :rename-files
-	   :sequence-from-file
-	   :sequence-to-file
-	   :find-in-file
-	   :find-in-directory
-	   :alter-file-contents
-	   :alter-directory-contents
-	   :concatenate-files
-	   :concatenate-and-remove-duplicates
-	   :remove-duplicate-lines-from-file
-	   :remove-duplicate-lines-from-string))
+   ;; strings
+   :whitespacep
+   :newlinep
+   :empty-string/newline-p
+   :ensure-string
+   :concat-string
+   :match-tokens
+   :match-token
+   :find-all
+   :explode-string
+   :map-exploding-string
+   :find-and-replace
+   :make-displaced-array
+
+   ;; files/directories/io
+   :walk-directory
+   :list-files
+   :directory-p
+   :file-p
+   :rename-files
+   :sequence-from-file
+   :sequence-to-file
+   :find-in-file
+   :find-in-directory
+   :alter-file-contents
+   :alter-directory-contents
+   :concatenate-files
+   :concatenate-and-remove-duplicates
+   :remove-duplicate-lines-from-file
+   :remove-duplicate-lines-from-string))
