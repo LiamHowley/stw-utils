10b6d91c48d9fcd50d6f86c34f0ef59942bd892d
Liam Howley <liam.howley@hotmail.com>
2022-12-26 00:52:32 +0000

    general update to reader functions, removing the unnecessary returning of functions in favour of run time function calls. Additional function match-strings which relies on the return values of match-string, both a search function and the generated trie.

--- lexer.lisp
+++ lexer.lisp
@@ -33,6 +33,7 @@
       (the character (char (the simple-string *document*) *char-index*))
       (the keyword :eof)))
 
+
 (defun next (&optional (increment 1))
   "Increments *CHAR-INDEX* by a supplied increment.
 If none is supplied, *CHAR-INDEX* increments by 1"
@@ -59,8 +60,7 @@ If none is supplied, *CHAR-INDEX* increments by 1"
     (char *document* (1- *char-index*))))
 
 
-
-(declaim (ftype (function (function) function)
+(declaim (ftype (function (function) (values fixnum fixnum))
 		consume-until
 		consume-while))
 
@@ -68,30 +68,30 @@ If none is supplied, *CHAR-INDEX* increments by 1"
   "Consumes while character/token challenge is met. Predicate is a 
 function that accepts one character. Returns the index of the first
 and last characters read."
-  #'(lambda (&optional (start *char-index*))
-      (declare (fixnum start)
-	       (optimize (safety 0) (speed 3)))
-      (loop
-	for char = (stw-read-char)
-	until (eq char :eof)
-	while (funcall predicate char)
-	do (next))
-      (values start *char-index*)))
+  (declare (optimize (safety 0) (speed 3)))
+  (let ((start *char-index*))
+    (declare (fixnum start))
+    (loop
+      for char = (stw-read-char)
+      until (eq char :eof)
+      while (funcall predicate char)
+      do (next))
+    (values start *char-index*)))
 
 
 (defun consume-until (predicate)
   "Consumes until character/token challenge is met. Predicate is a 
 function that accepts one character. Returns the index of the first
 and last characters read."
-  #'(lambda (&optional (start *char-index*))
-      (declare (fixnum start)
-	       (optimize (safety 0) (speed 3)))
-      (loop
-	for char = (stw-read-char)
-	until (or (eq char :eof)
-		  (funcall predicate char))
-	do (next))
-      (values start *char-index*)))
+  (declare (optimize (safety 0) (speed 3)))
+  (let ((start *char-index*))
+    (declare (fixnum start))
+    (loop
+      for char = (stw-read-char)
+      until (or (eq char :eof)
+		(funcall predicate char))
+      do (next))
+    (values start *char-index*)))
 
 
 (declaim (ftype (function () (values fixnum fixnum)) consume-whitespace)
@@ -111,33 +111,58 @@ whitespace characters. Returns the values
 
 
 (declaim (inline match-string)
-	 (ftype (function (simple-string &optional boolean trie) function)
+	 (ftype (function (simple-string &optional boolean trie) (values function trie))
 		match-string))
 
-
 (defun match-string (token &optional case-sensitive (trie (make-trie)))
-  (let ((length (length token)))
-    (declare (type trie trie) (fixnum length)
-	     (optimize (speed 3) (safety 0)))
-    (insert-word token trie token case-sensitive)
-    #'(lambda (&optional char)
-	(declare (ignore char)
-		 (inline trie-leaf)
-		 (optimize (speed 3) (safety 0)))
-	(let ((result)
-	      (foundp (walk-branch trie)))
-	  (declare (function foundp))
-	  (loop
-	     for index of-type fixnum from *char-index* below *length*
-	     for char = (the character (char *document* index))
-	     for next = (funcall foundp char)
-	     when next
+  "Adds the token to the trie as supplied by the &optional arg. 
+Returns both a function which searches the *DOCUMENT* string at
+*CHAR-INDEX* for a match within the trie, and the trie itself, 
+so that other tokens may be added to the trie."
+  (declare (optimize (speed 3) (safety 0))
+	   (type trie trie))
+  (insert-word token trie token case-sensitive)
+  (values 
+   #'(lambda (&optional char)
+       (declare (ignore char)
+		(inline trie-leaf)
+		(optimize (speed 3) (safety 0)))
+       (let ((result)
+	     (foundp (walk-branch trie)))
+	 (declare (function foundp))
+	 (loop
+	   for index of-type fixnum from *char-index* below *length*
+	   for char = (the character (char *document* index))
+	   for next = (funcall foundp char)
+	   when next
 	     do (setf result (trie-leaf next))
-	     unless next
+	   unless next
 	     do (return))
-	  (when result
-	    (prog1 *char-index*
-	      (next length)))))))
+	 (when result
+	   (prog1 *char-index*
+	     (next (length result))))))
+   trie))
+
+
+(declaim (ftype (function (list &optional boolean) (values function trie)) match-strings)
+	 (inline match-strings))
+
+(defun match-strings (tokens &optional case-sensitive)
+  "Takes tokens and retrieves a search function and trie from 
+the call to MATCH-STRING on the first token. Adds tokens to the 
+rest of the tokens to the trie.
+
+Returns both the search function which searches the *DOCUMENT* string at
+*CHAR-INDEX* for a match within the trie, and the trie itself, 
+so that other tokens may be added to the trie."
+  (declare (optimize (speed 3) (safety 0)))
+  (multiple-value-bind (func trie)
+      (match-string (car tokens))
+    (loop
+      for token in (cdr tokens)
+      do (insert-word token trie token case-sensitive))
+    (values func trie)))
+
 
 
 (declaim (inline match-character)
@@ -152,35 +177,35 @@ whitespace characters. Returns the values
 	*char-index*)))
 
 
-(declaim (ftype (function (function) function) read-until))
+
+(declaim (ftype (function (function) (values (or string null) character boolean)) read-until))
 
 (defun read-until (predicate)
-  "Creates and returns a reader function that reads until 
-character/token challenge is met and can be invoked as required. 
-Predicate is a function that accepts one character. Each time the
-reader is invoked, it returns a subseq of *DOCUMENT*, 
-the last char read and a boolean indicating EOF"
-  #'(lambda (&optional (start *char-index*))
-      (declare (optimize (safety 0) (speed 3))
-	       (fixnum start))
-      (loop 
-	 (let ((char (stw-read-char)))
-	   (etypecase char
-	     (character
-	      (let ((result (funcall (the function predicate) char)))
-		(cond ((and result (eql *char-index* start))
-		       ;; empty
-		       (return (values nil char nil)))
-		      (result
-		       (return (values (subseq *document* start result)
-				       char
-				       nil)))
-		      (t (next)))))
-	     (keyword 
-	      (return 
-		(values (subseq *document* start *char-index*)
-			(stw-peek-last-char)
-			t))))))))
+  "Reads until character/token challenge is met and can be invoked 
+as required. Predicate is a function that accepts one character. 
+Returns a subseq of *DOCUMENT*,the last char read and a boolean 
+indicating EOF"
+  (declare (optimize (safety 0) (speed 3)))
+  (let ((start *char-index*))
+    (declare (fixnum start))
+    (loop 
+      (let ((char (stw-read-char)))
+	(etypecase char
+	  (character
+	   (let ((result (funcall (the function predicate) char)))
+	     (cond ((and result (eql *char-index* start))
+		    ;; empty
+		    (return (values nil char nil)))
+		   (result
+		    (return (values (subseq *document* start result)
+				    char
+				    nil)))
+		   (t (next)))))
+	  (keyword 
+	   (return 
+	     (values (subseq *document* start *char-index*)
+		     (stw-peek-last-char)
+		     t))))))))
 
 
 
@@ -192,59 +217,53 @@ the last char read and a boolean indicating EOF"
 coordinates of the observed entity, and the decoded character
 as a result."
   (declare (optimize (safety 0) (speed 3)))
-  (multiple-value-bind (coordinates result)
+  (multiple-value-bind (character position)
       (funcall decoder *char-index*)
-    (when result
-      (setf *char-index* (cadr coordinates))
-      result)))
+    (when character
+      (setf *char-index* (the fixnum position))
+      character)))
 
 
-
-(declaim (ftype (function (&optional function) function)
-		read-and-decode
-		read-and-encode))
+(declaim (ftype (function (&optional function character) (values string character boolean))
+		read-and-decode))
 
 (defun read-and-decode (&optional (predicate (constantly nil)) (decode-char #\&))
   "Reads and decodes *DOCUMENT*, until EOF or PREDICATE. PREDICATE is tested
-against all chars, but those in encoded entities. Returns a reader function 
-to be invoked as required."
+against all chars, but those in encoded entities. Returns a decoded subseq of 
+*DOCUMENT*,the last char read and a boolean indicating EOF"
   (declare (optimize (safety 0) (speed 3))
 	   (inline make-displaced-array))
-  (let ((reader (read-until #'(lambda (char)
-				(when (or (funcall (the function predicate) char)
-					  (char= char decode-char))
-				  *char-index*)))))
-    #'(lambda ()
-	(let ((fragments)
-	      (start)
-	      (last))
-	  (declare (reader function))
-	  (loop
-	    (multiple-value-bind (token char eof)
-		(funcall reader)
-	      (setf start *char-index*)
-	      (cond ((eq char decode-char)
-		     (when token
-		       (push token fragments))
-		     (let ((character (decode *decoder*)))
-		       (if character
-			   (push character fragments)
-			   ;; test if char is matched by the predicate function.
-			   ;; If so process and return.
-			   (cond ((funcall (the function predicate) decode-char)
-				  (push (make-displaced-array *document* start *char-index*) fragments)
-				  (return (values (the simple-array (concat-string (nreverse fragments))) decode-char eof)))
-				 (t
-				  (next)
-				  (push (make-displaced-array *document*  start *char-index*) fragments)))))
-		     (setf last *char-index*))
-		    (t
-		     (cond (fragments
-			    (push (the string (make-displaced-array *document* last *char-index*)) fragments)
-			    (return (values (the simple-array (concat-string (nreverse fragments))) char eof)))
+  (let ((fragments)
+	(start)
+	(last))
+    (loop
+      (multiple-value-bind (token char eof)
+	  (read-until #'(lambda (char)
+			  (when (or (funcall (the function predicate) char)
+				    (char= char decode-char))
+			    *char-index*)))
+	(setf start *char-index*)
+	(cond ((eq char decode-char)
+	       (when token
+		 (push token fragments))
+	       (let ((character (decode *decoder*)))
+		 (if character
+		     (push character fragments)
+		     ;; test if char is matched by the predicate function.
+		     ;; If so process and return.
+		     (cond ((funcall (the function predicate) decode-char)
+			    (push (make-displaced-array *document* start *char-index*) fragments)
+			    (return (values (the simple-array (concat-string (nreverse fragments))) decode-char eof)))
 			   (t
-			    (return (values token char eof))))))))))))
-
+			    (next)
+			    (push (make-displaced-array *document* start *char-index*) fragments)))))
+	       (setf last *char-index*))
+	      (t
+	       (cond (fragments
+		      (push (the string (make-displaced-array *document* last *char-index*)) fragments)
+		      (return (values (the simple-array (concat-string (nreverse fragments))) char eof)))
+		     (t
+		      (return (values token char eof))))))))))
 
 
 (defmacro with-encoder (string encoder &body body)
@@ -254,46 +273,48 @@ to be invoked as required."
 	 (*encoder* ,encoder))
      ,@body))
 
+(declaim (ftype (function (&optional function function) (values string character boolean))
+		read-and-encode))
 
-(defun read-and-encode (&optional (encodep #'(lambda (char)
-					       (if (char= char #\&)
-						   (null (decode *decoder*))
-						   (member char '(#\' #\" #\< #\>))))))
-  "Returns a closure around the predicate encodep that reads and encodes characters within 
-*DOCUMENT* when the optional ENCODEP returns true. 
+(defun read-and-encode
+    (&optional
+       (predicate (constantly nil))
+       (encodep #'(lambda (char)
+		    (if (char= char #\&)
+			(null (decode *decoder*))
+			(member char '(#\' #\" #\< #\>))))))
+  "Accepts two optional args, a predicate function that accepts a character or string, 
+and determines a point of exit, and ENCODEP, a predicate of one character whose function 
+is to determine whether to encode or not. 
 
-ENCODEP is a predicate of one character whose function is to determine whether to encode or not.
 The default setting for encodep is to assume xml special characters are to be encoded. As xml
 special characters are parsed beginning with the #\& ampersand character, any #\& read is first tested
 with (decoder *DECODER*) to test for an already encoded character. If it is already encoded, it is 
 ignored. As this behaviour occurs within the body of the predicate ENCODEP, it is a trivial matter
 to change this behaviour to require double encoding.
 
-Within the returned closure a reader is formed from the return value of READ-UNTIL. This return value
-encapsulates a predicate of either ENCODEP or the predicate return-on-token."
-  (let ((test))
-    #'(lambda (&optional (return-on-token (constantly nil)))
-	(declare (optimize (safety 0) (speed 3)))
-	(let ((fragments)
-	      (reader (read-until #'(lambda (char)
-				      (when (or (setf test (funcall (the function return-on-token) char))
-						(funcall (the function encodep) char))
-					*char-index*)))))
-	  (loop
-	    (multiple-value-bind (token char eof)
-		(funcall reader)
-	      ;; catch special chars.
-	      (cond ((and fragments (or eof test))
-		     (when token
-		       (push token fragments))
-		     (return (values (the simple-array (concat-string (nreverse fragments))) char eof)))
-		    ((and char (or eof test))
-		     ;; no fragments means we've run through without needing to funcall *ENCODER*
-		     (return (values token char eof)))
-		    (char
-		     (when token
-		       (push token fragments))
-		     (push (the simple-array (funcall (the function *encoder*) char)) fragments)
-		     (next))
-		    (t
-		     (return (values token char eof))))))))))
+Returns an encoded subseq of *DOCUMENT*, the last char read and a boolean indicating EOF"
+  (declare (optimize (safety 0) (speed 3)))
+  (let ((test)
+	(fragments))
+    (loop
+      (multiple-value-bind (token char eof)
+	  (read-until #'(lambda (char)
+			  (when (or (setf test (funcall (the function predicate) char))
+				    (funcall (the function encodep) char))
+			    *char-index*)))
+	;; catch special chars.
+	(cond ((and fragments (or eof test))
+	       (when token
+		 (push token fragments))
+	       (return (values (the simple-array (concat-string (nreverse fragments))) char eof)))
+	      ((and char (or eof test))
+	       ;; no fragments means we've run through without needing to funcall *ENCODER*
+	       (return (values token char eof)))
+	      (char
+	       (when token
+		 (push token fragments))
+	       (push (the simple-array (funcall (the function *encoder*) char)) fragments)
+	       (next))
+	      (t
+	       (return (values token char eof))))))))
--- test/lexer.lisp
+++ test/lexer.lisp
@@ -6,49 +6,42 @@
   :parent stw-util
   (let* ((*document* "{\"accounts\" : [{\"id\" : 1, \"name\" : \"foo\"}]}")
 	 (*char-index* 0)
-	 (*length* (length *document*))
-	 (next-quote (consume-until (match-character #\")))
-	 (return-attr (read-until (match-character #\")))
-	 (return-value (read-until (match-character #\" #\,)))
-	 (consume-whitespace (consume-while #'whitespacep))
-	 (array-tag (consume-until (match-character #\[ #\])))
-	 (object-tag (consume-until (match-character #\{ #\})))
-	 (match-delimiter (consume-until (match-character #\:))))
+	 (*length* (length *document*)))
     (is char= #\{ (stw-read-char))
     (is char= #\" (stw-peek-next-char))
     (is eql 1 (next))
     (is eql 1 *char-index*)
     (is char= #\{ (stw-peek-last-char))
     (next)
-    (is string= "accounts" (funcall return-attr))
+    (is string= "accounts" (read-until (match-character #\")))
     (next)
-    (funcall consume-whitespace)
+    (consume-while #'whitespacep)
     (is char= #\: (stw-read-char))
-    (funcall array-tag)
+    (consume-until (match-character #\[ #\]))
     (is char= #\[ (stw-read-char))
-    (funcall object-tag)
+    (consume-until (match-character #\{ #\}))
     (is char= #\{ (stw-read-char))
-    (funcall next-quote)
+    (consume-until (match-character #\"))
     (is char= #\" (stw-read-char))
     (next)
-    (is string= "id" (funcall return-value))
-    (funcall match-delimiter)
+    (is string= "id" (read-until (match-character #\" #\,)))
+    (consume-until (match-character #\:))
     (next)
-    (funcall consume-whitespace)
-    (is string= "1" (funcall return-value))
-    (funcall next-quote)
+    (consume-while #'whitespacep)
+    (is string= "1" (read-until (match-character #\" #\,)))
+    (consume-until (match-character #\"))
     (next)
-    (is string= "name" (funcall return-value))
-    (funcall match-delimiter)
+    (is string= "name" (read-until (match-character #\" #\,)))
+    (consume-until (match-character #\:))
     (next)
-    (funcall consume-whitespace)
+    (consume-while #'whitespacep)
     (next)
-    (is string= "foo" (funcall return-value))
-    (funcall object-tag)
+    (is string= "foo" (read-until (match-character #\" #\,)))
+    (consume-until (match-character #\{ #\}))
     (is char= #\} (stw-read-char))
-    (funcall array-tag)
+    (consume-until (match-character #\[ #\]))
     (is char= #\] (stw-read-char))
-    (funcall object-tag)
+    (consume-until (match-character #\{ #\}))
     (is char= #\} (stw-read-char))))
 
 
@@ -80,68 +73,63 @@
 			    when value
 			      do (setf result value)
 			    while next
-			    finally (return (values (list index i) result)))))))
-	 (decode-till-a (read-and-decode (match-character #\a)))
-	 (decode-till-space (read-and-decode (match-character #\space)))
-	 (decode-till-colour (read-and-decode (match-string "colour")))
-	 (decode-till-end (read-and-decode)))
-    (is string= "Just <" (funcall decode-till-a))
+			    finally (return (values result i))))))))
+    (is string= "Just <" (read-and-decode (match-character #\a)))
     (is char= #\a (stw-read-char))
     (next 2)
     (is char= #\h (stw-read-char))
-    (is string= "href=\"/url.com\"" (funcall decode-till-space))
+    (is string= "href=\"/url.com\"" (read-and-decode (match-character #\space)))
     (is char= #\space (stw-read-char))
     (next)
-    (is string= "target='_blank' class='class colour" (funcall decode-till-colour))
-    (is string= " position'>" (funcall decode-till-end))
+    (is string= "target='_blank' class='class colour" (read-and-decode (match-string "colour")))
+    (is string= " position'>" (read-and-decode))
     (let ((*char-index* 0))
       (is string=
 	  "Just <a href=\"/url.com\" target='_blank' class='class colour position'>"
-	  (funcall decode-till-end)))))
+	  (read-and-decode)))))
 
 
 (define-test encoding
   :parent stw-util
-(with-encoder
-  "Just <a href=\"/url.com\" target='_blank&#39; class='class colour position'>"
-  #'(lambda (char)
-      (cdr (assoc char *special-entities* :test #'char=)))
-  (let* ((*decoder* (let* ((trie (make-trie)))
-		      (loop
-			for (char . entity) in *special-entities*
-			do (insert-word entity trie char))
-		      (lambda (index)
-			(let ((foundp (walk-branch trie)))
-			  (loop
-			    with result = nil
-			    for i from index below *length*
-			    for char = (aref *document* i)
-			    for next = (funcall foundp char)
-			    for value = (when next
-					  (trie-leaf next))
-			    when value
-			      do (setf result value)
-			    while next
-			    finally (return (values (list index i) result)))))))
-	 (reader (read-and-encode)))
-    (is string=
-	"Just &lt;"
-	(funcall reader (match-character #\a)))
-    (is char= #\a (stw-read-char))
-    (next 2)
-    (is char= #\h (stw-read-char))
-    (is string=
-	"href=&quot;/url.com&quot;"
-	(funcall reader (match-character #\space)))
-    (is char= #\space (stw-read-char))
-    (next)
-    (is string=
-	"target=&#39;_blank&#39; class=&#39;class colour"
-	(funcall reader (match-string "colour")))
-    (is string=
-	" position&#39;&gt;"
-	(funcall reader))
-    (let ((*char-index* 0))
+  (with-encoder
+      "Just <a href=\"/url.com\" target='_blank&#39; class='class colour position'>"
+      #'(lambda (char)
+	  (cdr (assoc char *special-entities* :test #'char=)))
+    (let* ((*decoder* (let* ((trie (make-trie)))
+			(loop
+			  for (char . entity) in *special-entities*
+			  do (insert-word entity trie char))
+			(lambda (index)
+			  (let ((foundp (walk-branch trie)))
+			    (loop
+			      with result = nil
+			      for i from index below *length*
+			      for char = (aref *document* i)
+			      for next = (funcall foundp char)
+			      for value = (when next
+					    (trie-leaf next))
+			      when value
+				do (setf result value)
+			      while next
+			      finally (return (values result i))))))))
       (is string=
-	  "Just &lt;a href=&quot;/url.com&quot; target=&#39;_blank&#39; class=&#39;class colour position&#39;&gt;"
-	  (funcall reader))))))
+	  "Just &lt;"
+	  (read-and-encode (match-character #\a)))
+      (is char= #\a (stw-read-char))
+      (next 2)
+      (is char= #\h (stw-read-char))
+      (is string=
+	  "href=&quot;/url.com&quot;"
+	  (read-and-encode (match-character #\space)))
+      (is char= #\space (stw-read-char))
+      (next)
+      (is string=
+	  "target=&#39;_blank&#39; class=&#39;class colour"
+	  (read-and-encode (match-string "colour")))
+      (is string=
+	  " position&#39;&gt;"
+	  (read-and-encode))
+      (let ((*char-index* 0))
+	(is string=
+	    "Just &lt;a href=&quot;/url.com&quot; target=&#39;_blank&#39; class=&#39;class colour position&#39;&gt;"
+	    (read-and-encode))))))
