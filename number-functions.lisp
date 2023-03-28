9d1b4f1bb785252109e9b673b65df1a48fb1be5b
Liam Howley <liam.howley@hotmail.com>
2023-03-28 16:08:01 +0100

    set default optional param to 0s0 in get-float and allow double-float when read-from-string returns a single-float

--- number-functions.lisp
+++ number-functions.lisp
@@ -1,15 +1,12 @@
 (in-package stw.util)
 
 
-(declaim (ftype (function (string &optional float) float) float%)
-	 (inline float%))
+(declaim (ftype (function (string &optional (or single-float double-float)) (or single-float double-float)) get-float)
+	 (inline get-float))
 
-(defun get-float (string &optional prototype)
-  (declare (optimize (speed 3)(safety 0)))
-  (let ((num (read-from-string value)))
+(defun get-float (string &optional (prototype 0s0))
+  (let ((num (read-from-string string)))
     (the float 
 	 (etypecase num
-	   (float num)
-	   (number (aif prototype
-			(float num self)
-			(float num)))))))
+	   (float (float num prototype))
+	   (fixnum (float num prototype))))))
